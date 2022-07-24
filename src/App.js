3009bf81727cff52bae98fac84e09d518bf56166
Sabbir Ahmmed <ahsabbir103@gmail.com>
2022-07-24 12:51:14 +0600

    setup react-query

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useQuery } from "react-query";
+import "./App.css";
+import logo from "./logo.svg";
+import { products } from "./Query/Query";
+import { useState, useEffect } from "react";
 function App() {
+  const { data, error, isError, isLoading } = useQuery("products", products);
+
+  if (isError) return <h1>Error : {error.message} , Try again !</h1>;
+  if (isLoading) return <h1>Loading...</h1>;
+  console.log(data);
+  // if (isError) {
+  //   console.log(error.message);
+  // }
+
+  // const [Products, setProducts] = useState([]);
+
+  // useEffect(() => {
+  //   // products().then((data) => setProducts(data));
+  //   data && console.log(data);
+  // }, []);
+
   return (
     <div className="App">
+      {/* {Products && console.log(Products)} */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
